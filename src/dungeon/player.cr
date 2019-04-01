ff9749bb438adedbbf2dbe2047cfd86b62db0d98
Matt Swieboda <matts@topsteptrader.com>
2019-04-01 14:00:31 -0500

    refactors player attack to weapon class

--- src/dungeon/player.cr
+++ src/dungeon/player.cr
@@ -5,21 +5,14 @@ module Dungeon
     include DirectionTextures
 
     property tint : LibRay::Color
-    property attacking : Bool
-    property attack_time : Int32
-    property attack_x : Float32
-    property attack_y : Float32
-    property attack_rotation : Float32
     property enemy_bump_flash_time : Int32
     property invincible_time : Int32
+    property weapon : Weapon
 
     FADED = LibRay::Color.new(r: 255, g: 255, b: 255, a: 100)
 
     PLAYER_MOVEMENT = 200
     TINT_DEFAULT    = LibRay::WHITE
-
-    ATTACK_TIME   = 15
-    ATTACK_FRAMES =  5
 
     ENEMY_BUMP_FLASH_TIME     = 15
     ENEMY_BUMP_FLASH_INTERVAL =  5
@@ -38,16 +31,10 @@ module Dungeon
 
       @tint = TINT_DEFAULT
 
-      @attacking = false
-      @attack_time = 0
-      @attack_sprites = [] of LibRay::Texture2D
-      @attack_x = @attack_y = @attack_rotation = 0
+      @weapon = Weapon.new(loc: loc, direction: @direction, x_offset: width / 2, y_offset: height / 2)
 
       @enemy_bump_flash_time = 0
       @invincible_time = 0
-
-      image = LibRay.load_image(File.join(__DIR__, "assets/player-attack.png"))
-      @attack_sprite = LibRay.load_texture_from_image(image)
     end
 
     def texture_file_name
@@ -55,29 +42,7 @@ module Dungeon
     end
 
     def draw
-      if attacking?
-        LibRay.draw_texture_pro(
-          texture: @attack_sprite,
-          source_rec: LibRay::Rectangle.new(
-            x: 0,
-            y: attack_frame * @attack_sprite.height / ATTACK_FRAMES,
-            width: @attack_sprite.width,
-            height: @attack_sprite.height / ATTACK_FRAMES
-          ),
-          dest_rec: LibRay::Rectangle.new(
-            x: x + attack_x,
-            y: y + attack_y,
-            width: @attack_sprite.width,
-            height: @attack_sprite.height / ATTACK_FRAMES
-          ),
-          origin: LibRay::Vector2.new(
-            x: @attack_sprite.width / 2,
-            y: @attack_sprite.height / ATTACK_FRAMES / 2
-          ),
-          rotation: attack_rotation,
-          tint: tint
-        )
-      end
+      weapon.draw
 
       LibRay.draw_texture_v(
         texture: direction_textures[direction.value],
@@ -88,48 +53,7 @@ module Dungeon
         tint: tint
       )
 
-      if draw_collision_box?
-        draw_collision_box
-        draw_collision_box(attack_collision_box) if attacking?
-      end
-    end
-
-    def attacking?
-      attacking
-    end
-
-    def attack
-      @attack_time = 0
-      @attacking = true
-    end
-
-    def attack_frame
-      (@attack_time / (ATTACK_TIME / ATTACK_FRAMES)).to_i
-    end
-
-    def attack_collision_box
-      box_x = 0
-      box_y = 0
-      box_width = 0
-      box_height = 0
-
-      if direction.up? || direction.down?
-        box_x = attack_x - @attack_sprite.width / 2
-        box_y = attack_y - (@attack_sprite.height / ATTACK_FRAMES / 2)
-        box_width = @attack_sprite.width
-        box_height = @attack_sprite.height / ATTACK_FRAMES
-      elsif direction.left? || direction.right?
-        box_x = attack_x - (@attack_sprite.height / ATTACK_FRAMES / 2)
-        box_y = attack_y - @attack_sprite.width / 2
-        box_width = @attack_sprite.height / ATTACK_FRAMES
-        box_height = @attack_sprite.width
-      end
-
-      Box.new(
-        loc: Location.new(box_x, box_y),
-        width: box_width,
-        height: box_height
-      )
+      draw_collision_box if draw_collision_box?
     end
 
     def enemy_bump(enemies)
@@ -205,35 +129,9 @@ module Dungeon
         @loc.x -= delta if collisions?(entities)
       end
 
-      attack if !attacking? && LibRay.key_pressed?(LibRay::KEY_SPACE)
-
-      # attack timer
-      if attacking?
-        @attack_time += 1
-
-        if attack_time >= ATTACK_TIME
-          @attacking = false
-        end
-      end
-
-      if attacking?
-        @attack_x = @attack_y = @attack_rotation = 0
-
-        if direction.up?
-          @attack_y = -height / 2
-        elsif direction.down?
-          @attack_y = height / 2
-          @attack_rotation = 180
-        elsif direction.left?
-          @attack_x = -width / 2
-          @attack_rotation = -90
-        elsif direction.right?
-          @attack_x = width / 2
-          @attack_rotation = 90
-        end
-
-        # TODO: check for collisions with sword with enemies
-      end
+      weapon.direction = direction
+      weapon.loc = loc
+      weapon.movement(entities)
     end
   end
 end
---
+++ src/dungeon/weapon.cr
@@ -0,0 +1,123 @@
+module Dungeon
+  class Weapon < Entity
+    property direction : Direction
+    property tint : LibRay::Color
+    property x_offset : Float32
+    property y_offset : Float32
+
+    TINT_DEFAULT = LibRay::WHITE
+
+    ATTACK_TIME   = 15
+    ATTACK_FRAMES =  5
+
+    def initialize(@loc : Location, @direction : Direction, @x_offset : Float32, @y_offset : Float32)
+      @tint = TINT_DEFAULT
+      @attacking = false
+      @attack_time = 0
+      @attack_sprites = [] of LibRay::Texture2D
+      @attack_x = @attack_y = @attack_rotation = 0_f32
+      image = LibRay.load_image(File.join(__DIR__, "assets/player-attack.png"))
+      @attack_sprite = LibRay.load_texture_from_image(image)
+
+      collision_box = Box.new(Location.new(0, 0), width: @attack_sprite.width, height: @attack_sprite.width / ATTACK_FRAMES)
+
+      super(@loc, @attack_sprite.width, @attack_sprite.height, collision_box)
+    end
+
+    def draw
+      return unless attacking?
+
+      LibRay.draw_texture_pro(
+        texture: @attack_sprite,
+        source_rec: LibRay::Rectangle.new(
+          x: 0,
+          y: attack_frame * @attack_sprite.height / ATTACK_FRAMES,
+          width: @attack_sprite.width,
+          height: @attack_sprite.height / ATTACK_FRAMES
+        ),
+        dest_rec: LibRay::Rectangle.new(
+          x: x + @attack_x,
+          y: y + @attack_y,
+          width: @attack_sprite.width,
+          height: @attack_sprite.height / ATTACK_FRAMES
+        ),
+        origin: LibRay::Vector2.new(
+          x: @attack_sprite.width / 2,
+          y: @attack_sprite.height / ATTACK_FRAMES / 2
+        ),
+        rotation: @attack_rotation,
+        tint: tint
+      )
+
+      draw_collision_box if draw_collision_box?
+    end
+
+    def attacking?
+      @attacking
+    end
+
+    def attack
+      @attack_time = 0
+      @attacking = true
+    end
+
+    def attack_frame
+      (@attack_time / (ATTACK_TIME / ATTACK_FRAMES)).to_i
+    end
+
+    def movement(entities)
+      attack if !attacking? && LibRay.key_pressed?(LibRay::KEY_SPACE)
+      return unless attacking?
+
+      # timer
+      @attack_time += 1
+
+      if @attack_time >= ATTACK_TIME
+        @attacking = false
+      end
+
+      @attack_x = @attack_y = @attack_rotation = 0
+
+      if direction.up?
+        @attack_y = -y_offset
+      elsif direction.down?
+        @attack_y = y_offset
+        @attack_rotation = 180
+      elsif direction.left?
+        @attack_x = -x_offset
+        @attack_rotation = -90
+      elsif direction.right?
+        @attack_x = x_offset
+        @attack_rotation = 90
+      end
+
+      # TODO: check for collisions with weapon with enemies
+
+      # change collision box based on direction
+      box_x = 0
+      box_y = 0
+      box_width = 0
+      box_height = 0
+
+      if direction.up? || direction.down?
+        box_x = @attack_x - @attack_sprite.width / 2
+        box_y = @attack_y - (@attack_sprite.height / ATTACK_FRAMES / 2)
+        box_width = @attack_sprite.width
+        box_height = @attack_sprite.height / ATTACK_FRAMES
+      elsif direction.left? || direction.right?
+        box_x = @attack_x - (@attack_sprite.height / ATTACK_FRAMES / 2)
+        box_y = @attack_y - @attack_sprite.width / 2
+        box_width = @attack_sprite.height / ATTACK_FRAMES
+        box_height = @attack_sprite.width
+      end
+
+      # change weapon loc, and collision_box
+      @collision_box.tap do |box|
+        box.x = box_x.to_f32
+        box.y = box_y.to_f32
+        box.width = box_width.to_f32
+        box.height = box_height.to_f32
+      end
+    end
+  end
+end
